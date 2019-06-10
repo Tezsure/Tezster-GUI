d15a1a146bbabbc618e01fea0471a5172483e359
rmanjhi <rmanjhi.cse13@nituk.ac.in>
2019-06-10 19:39:24 +0530

    code changes for localnode

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpRequest, HttpHandler, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import {configDataTpye} from '../assets/configTypeData';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
 
 
 const apiURLCall="http://localhost:18731";
@@ -12,7 +13,11 @@ private _url: string='/assets/config.json';
   constructor(private http:HttpClient) { }
 
   getConfigData():Observable<configDataTpye[]>{
-    return this.http.get<configDataTpye[]>(this._url);   
+    return this.http.get<configDataTpye[]>(this._url).catch(this.errorHandler);   
   }
+
+  errorHandler(error : HttpErrorResponse){
+    return Observable.throw(error.message || "Server Error");
+  };
 
 }
