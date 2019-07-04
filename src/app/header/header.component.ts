2235568cad64dfb971f44d8d6be74a0bd1b8e189
rajeev7 <rajeev@tezsure.com>
2019-07-04 10:01:30 +0530

    add header menus

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {LocalnodeComponent} from '../localnode/localnode.component';
+import { ModalService } from '../modal.service';
+
 
 @Component({
   selector: 'app-header',
@@ -6,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  //provider: string;
+  constructor(private modalService: ModalService) { 
+  }
+  openDialog(): void {
+    /* const dialogRef = this.dialog.open(LocalnodeComponent, {
+      width: '400px',
+      data: {provider: this.provider}
+    });
 
-  constructor() { }
+    dialogRef.afterClosed().subscribe(result => {
+      this.provider = result;
+    }); */
+    this.modalService.openModal('localnode', LocalnodeComponent);
 
+  }
   ngOnInit() {
   }
 
