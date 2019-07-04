f165f715fa35efb7d4439b007a59268d2909a887
rajeev7 <rajeev@tezsure.com>
2019-07-04 10:00:15 +0530

    add services to storing the data/modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+declare var eztz: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tezsure';
+  ngOnInit():void{    
+    // console.log(JSON.stringify(eztz.crypto.generateKeys()));
+  }
+  
 }
