5f92b71f25759529b223750239bdb686d6551acb
rajeev7 <rajeev@tezsure.com>
2019-07-09 10:09:59 +0530

    add services localstorage data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule,routingComponent} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import {AppService} from './app.service';
-import {HttpClientModule } from '@angular/common/http';
+import { AppService } from './app.service';
+import { HttpClientModule } from '@angular/common/http';
 import { AlphanetComponent } from './alphanet/alphanet.component';
 import { WalletComponent } from './wallet/wallet.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import {LocalnodeComponent} from './localnode/localnode.component';
+import { LocalnodeComponent } from './localnode/localnode.component';
 import { FormsModule } from '@angular/forms';
 import { CreateWalletComponent } from './create-wallet/create-wallet.component';
 import { RestoreWalletComponent } from './restore-wallet/restore-wallet.component';
@@ -17,12 +17,12 @@ import { BlocksComponent } from './blocks/blocks.component';
 import { ContractsComponent } from './contracts/contracts.component';
 import { EventsComponent } from './events/events.component';
 import { TransactionComponent } from './transaction/transaction.component';
+import { AccountsComponent } from './accounts/accounts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,    
-    routingComponent,
+    HeaderComponent,
     AlphanetComponent,
     WalletComponent,
     LocalnodeComponent,
@@ -31,18 +31,18 @@ import { TransactionComponent } from './transaction/transaction.component';
     BlocksComponent,
     ContractsComponent,
     EventsComponent,
-    TransactionComponent    
+    AccountsComponent,
+    TransactionComponent
   ],
-  entryComponents: [LocalnodeComponent,CreateWalletComponent,RestoreWalletComponent,AlphanetComponent],
+  entryComponents: [LocalnodeComponent, CreateWalletComponent, RestoreWalletComponent, AlphanetComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
     FormsModule,
     ModalModule.forRoot()
   ],
-    
   providers: [AppService],
   bootstrap: [AppComponent]
 })
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,57 +1,72 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpRequest, HttpHandler, HttpParams, HttpErrorResponse } from '@angular/common/http';
-import {configDataTpye} from '../assets/configTypeData';
-import {Observable, BehaviorSubject, ArgumentOutOfRangeError} from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { configDataTpye } from '../assets/configTypeData';
+import { Observable, BehaviorSubject } from 'rxjs';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
 declare var eztz: any;
 @Injectable()
 export class AppService {
-  private _url: string='/assets/config.json';
+    private _url: string = '/assets/config.json';
 
-  protected _configData: any;
-  public amount: any;
-  public configDataChangeObs$: BehaviorSubject<any>;
+    protected _configData: any;
+    public amount: any;
+    public configDataChangeObs$: BehaviorSubject<any>;
 
-  constructor(private http:HttpClient) { 
-    this.configDataChangeObs$ = new BehaviorSubject<any>(null);
-  }
+    constructor(private http: HttpClient) {
+        this.configDataChangeObs$ = new BehaviorSubject<any>(null);
+    }
 
-  getConfigData():Observable<configDataTpye[]>{
-    const ajax = this.http.get<configDataTpye[]>(this._url).catch(this.errorHandler);   
-    ajax.subscribe(data => this.configData = data);
-    return ajax;
-  }
+    fetchConfigData(): Observable<configDataTpye[]> {
+        const ajax = this.http.get<configDataTpye[]>(this._url).catch(this.errorHandler);
+        ajax.subscribe(this._onConfigDataResponse.bind(this));
+        return ajax;
+    }
 
-  errorHandler(error : HttpErrorResponse){
-    return Observable.throw(error.message || "Server Error");
-  };
-  public addAccount(data: { "label": string; "pkh": string; "identity": string; }) {
-    this.configData.accounts.push(data);
-    console.log("addCont", this.configData);
-  }
+    protected _onConfigDataResponse(data) {
+        this.configData = data;
+        console.log("data is: " + data);
+        this.setLocalConfigData(data);
+    }
 
-  public setProviderData(data:string) {    
-    this.configData.provider=data;
-    sessionStorage.setItem("rpc-node ",data);      
-  }
-  public get configData() {
-    return this._configData;
-  }
+    public setLocalConfigData(data) {
+        localStorage.setItem('configData', data);
+    }
 
-  public set configData(data) {
-    this._configData = data;
-    this.configDataChangeObs$.next(data);
-  }
+    public getLocalConfigData() {
+        return localStorage.getItem('configData');
+    }
 
-  public getBalance(keys:string) {
-    eztz.rpc.getBalance(keys).then(function (res: number) {    
-      return (res/1000000).toFixed(3)+" Tz";        
-    }).catch(function (e: any) {
-      return 
-    });       
-       
-  }
+    errorHandler(error: HttpErrorResponse) {
+        return Observable.throw(error.message || 'Server Error');
+    };
+    public addAccount(data: { 'label': string; 'pkh': string; 'identity': string; }) {
+        this.configData.accounts.push(data);
+    }
+
+    public setProviderData(data: string) {
+        this.configData.provider = data;
+    }
+    public get configData() {
+        return this._configData;
+    }
+
+    public set configData(data) {
+        this._configData = data;
+        this.configDataChangeObs$.next(data);
+    }
+
+    public getBalance(keys: string) {
+        return new Promise((resolve, reject) => {
+            eztz.rpc.getBalance(keys).then( (res: number) => {
+                resolve((res / 1000000).toFixed(3) + ' Tz');
+            }).catch( (e: any) => {
+                console.log(e);
+                reject();
+            });
+        });
+
+    }
 
 }
