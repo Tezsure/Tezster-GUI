c76c1d7fd4b0d2404253c9f3ecbdcae0b126cd93
rajeev7 <rajeev@tezsure.com>
2019-08-30 16:59:44 +0530

    code changes for UI

--- src/app/blocks/blocks.component.ts
+++ src/app/blocks/blocks.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-blocks',
-  templateUrl: './blocks.component.html',
-  styleUrls: ['./blocks.component.css']
+    selector: 'app-blocks',
+    templateUrl: './blocks.component.html',
+    styleUrls: ['./blocks.component.css']
 })
 export class BlocksComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/contracts/contracts.component.ts
+++ src/app/contracts/contracts.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contracts',
-  templateUrl: './contracts.component.html',
-  styleUrls: ['./contracts.component.css']
+    selector: 'app-contracts',
+    templateUrl: './contracts.component.html',
+    styleUrls: ['./contracts.component.css']
 })
 export class ContractsComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
