6fed320f4a34e3fd777f2f7d0a0b3ca3c9cfca87
rajeev7 <rajeev@tezsure.com>
2019-08-30 16:44:29 +0530

    add account label/UI fixed

--- src/app/validate-wallet/validate-wallet.component.ts
+++ src/app/validate-wallet/validate-wallet.component.ts
@@ -19,8 +19,10 @@ export class ValidateWalletComponent implements OnInit {
   	public email;  
   	public cred:string;
   	public keys;
-  	public account;
-  	credD: { "pkh": any; "sk": any; "pk": any; type: any };	
+	public account;
+	public label;
+	public obj;
+  	credD: { "pkh": any; "sk": any; "pk": any; label: any };	
   	constructor(public bsModalRef: BsModalRef, private _AppService:AppService, private modalService: ModalService,private router: Router ) { }
   	onNoClick(): void {     
   		this.modalService.closeModal('validate');
@@ -29,16 +31,21 @@ export class ValidateWalletComponent implements OnInit {
 	onClick(): void {      
  		this.cred=this.email+this.password;  
   		this.keys=eztz.crypto.generateKeys(this.mnenomics, this.cred); 
-  		this.account=this.keys.pkh;         
+		this.account=this.keys.pkh; 
+		// eztz.rpc.activate(this.keys.pkh, this.secret).then(function(d){		
+		// 	console.log(d);
+		// });          
   		this._AppService.addAccount(
 			{
-		  		"label": "bootstrap_7_localNode",
+		  		"label": this.label,
 		  		"pkh": this.account,
-				"identity": "bootstrap7_localNode"
+				"identity": this.label
 			}
 	  	);      
-  		this.credD={"pkh":this.keys.pkh,"sk":this.keys.sk,"pk":this.keys.pk , type : "encrypted"};
-    	console.log(JSON.stringify(this.credD));
+  		this.credD={"pkh":this.keys.pkh,"sk":this.keys.sk,"pk":this.keys.pk ,"label":this.label};		
+		this.obj=JSON.parse(this._AppService.getLocalConfigData());
+        this.obj.identities.push(this.credD);
+        this._AppService.setLocalConfigData(this.obj);
 		this.modalService.closeModal('validate');
 		this.modalService.closeModal('create');
 		this.router.navigate(['/accounts']);    
