461b5db51ed89976f9aa00ce4c9b307b77f8e405
rajeev7 <rajeev@tezsure.com>
2019-08-30 17:03:29 +0530

    code changes for UI fixed+account label

--- src/app/create-wallet/create-wallet.component.ts
+++ src/app/create-wallet/create-wallet.component.ts
@@ -8,24 +8,24 @@ import { ValidateWalletComponent } from '../validate-wallet/validate-wallet.comp
 declare var eztz: any;
 
 @Component({
-    selector: 'app-create-wallet',
-    templateUrl: './create-wallet.component.html',
-    styleUrls: ['./create-wallet.component.css']
+      selector: 'app-create-wallet',
+      templateUrl: './create-wallet.component.html',
+      styleUrls: ['./create-wallet.component.css']
 })
 export class CreateWalletComponent implements OnInit {
-    public configData=[];
-    public mnemonics: any;
-    public showSeed='';
-    public mm: any;
-    constructor(public bsModalRef: BsModalRef, private _AppService:AppService, private modalService: ModalService ) { }
+      public configData=[];
+      public mnemonics: any;
+      public showSeed='';
+      public mm: any;
+      constructor(public bsModalRef: BsModalRef, private _AppService:AppService, private modalService: ModalService ) { }
     
     
     onNoClick(): void {     
-    	this.modalService.closeModal('create');
+    	  this.modalService.closeModal('create');
     }
   
    onClick(): void {   
-    	this.modalService.openModal('validate', ValidateWalletComponent); 
+    	  this.modalService.openModal('validate', ValidateWalletComponent); 
   	}
   	newMnemonic():void{
         this.mnemonics=eztz.crypto.generateMnemonic();    
