83ea87c8c31029e9aff11d53cb6d810ef31901ac
rajeev7 <rajeev@tezsure.com>
2019-08-30 16:58:36 +0530

    code changes for UI changes

--- src/app/blank-data/blank-data.component.ts
+++ src/app/blank-data/blank-data.component.ts
@@ -3,19 +3,19 @@ import { BsModalRef} from 'ngx-bootstrap/modal';
 import { ModalService } from '../modal.service';
 
 @Component({
-  selector: 'app-blank-data',
-  templateUrl: './blank-data.component.html',
-  styleUrls: ['./blank-data.component.css']
+	selector: 'app-blank-data',
+	templateUrl: './blank-data.component.html',
+	styleUrls: ['./blank-data.component.css']
 })
 export class BlankDataComponent implements OnInit {
 
-  constructor(public bsModalRef: BsModalRef,private modalService: ModalService ) { }
+	constructor(public bsModalRef: BsModalRef,private modalService: ModalService ) { }
 
-  onClick(): void {       
-    this.modalService.closeModal('blankData');
-  }
+	onClick(): void {       
+		this.modalService.closeModal('blankData');
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
