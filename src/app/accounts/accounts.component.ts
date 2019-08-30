76d20bb7aa82ac0dcae05f2ceeda7e5003904bee
rajeev7 <rajeev@tezsure.com>
2019-08-30 16:57:28 +0530

    code changes for balance for accounts and add UI-Button for create restore wallet

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -9,46 +9,46 @@ import 'rxjs/add/observable/throw';
 declare var eztz: any;
 
 @Component({
-  selector: 'app-accounts',
-  templateUrl: './accounts.component.html',
-  styleUrls: ['./accounts.component.css']
+	selector: 'app-accounts',
+	templateUrl: './accounts.component.html',
+	styleUrls: ['./accounts.component.css']
 })
 export class AccountsComponent implements OnInit {
 
-  public configData = null;
-  public acc=[];
-  balance: any;
-  constructor(private _AppService: AppService,private modalService: ModalService) { }
-  create(): void {    
-    this.modalService.openModal('create', CreateWalletComponent);
-  }
-  restore(): void {
-    this.modalService.openModal('restore', RestoreWalletComponent);
-  }
+	public configData = null;
+	public acc=[];
+	balance: any;
+	constructor(private _AppService: AppService,private modalService: ModalService) { }
+	create(): void {    
+		this.modalService.openModal('create', CreateWalletComponent);
+	}
+	restore(): void {
+		this.modalService.openModal('restore', RestoreWalletComponent);
+	}
 
-  ngOnInit() {
-    this._AppService.configDataChangeObs$
-      .subscribe(data => {
-        if (data) {
-          this.configData = data;          
-          eztz.node.setProvider(this.configData.provider);
-          for(var accounts of this.configData.accounts){            
-            this.acc.push({
-              "label" :accounts.label,
-              "address" : accounts.pkh,
-              "balance" : this.getBalance(accounts.pkh),
-              "txc" :0,
-              "index" :0,
-              "sec":accounts.pkh              
-            }); 
-          }          
-        }
-      });      
-  }
-  async getBalance(pkh: any) {       
-     this.balance= await this._AppService.getBalance(pkh);      
-     return this.balance;       
-  }
-  
+	ngOnInit() {    
+		this._AppService.configDataChangeObs$
+		.subscribe(data => {
+			if (data) {
+			this.configData = data;          
+			eztz.node.setProvider(this.configData.provider);
+			for(var accounts of this.configData.accounts){            
+				this.acc.push({
+				"label" :accounts.label,
+				"address" : accounts.pkh,
+				"balance" : this.getBalance(accounts.pkh),
+				"txc" :0,
+				"index" :0,
+				"sec":accounts.pkh              
+				}); 
+			}          
+			}
+		});      
+	}
+	async getBalance(pkh: any) {       
+		this.balance= await this._AppService.getBalance(pkh);      
+		return this.balance;       
+	}
+	  
 }
 
