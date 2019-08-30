6102bb9c862c16317a24c0bc4ddb0c13f62c8057
rajeev7 <rajeev@tezsure.com>
2019-08-30 17:00:09 +0530

    code changes for UI

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-events',
-  templateUrl: './events.component.html',
-  styleUrls: ['./events.component.css']
+    selector: 'app-events',
+    templateUrl: './events.component.html',
+    styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
