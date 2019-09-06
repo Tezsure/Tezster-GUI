502562e98d7eed8bdf7bca302efb63cddbdbfd8b
rajeev7 <rajeev@tezsure.com>
2019-09-06 15:00:42 +0530

    update wallet

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -5,31 +5,30 @@ import { CreateWalletComponent } from '../create-wallet/create-wallet.component'
 import { RestoreWalletComponent } from '../restore-wallet/restore-wallet.component';
 
 @Component({
-  selector: 'app-wallet',
-  templateUrl: './wallet.component.html',
-  styleUrls: ['./wallet.component.css']
+    selector: 'app-wallet',
+    templateUrl: './wallet.component.html',
+    styleUrls: ['./wallet.component.css']
 })
 export class WalletComponent implements OnInit {
-  public configData = [];
+    public configData = [];
 
-  constructor(private _AppService: AppService, private modalService: ModalService) { }
-  create(): void {
-    this.modalService.openModal('create', CreateWalletComponent);
-  }
+    constructor(private _AppService: AppService, private modalService: ModalService) { }
+    create(): void {   
+        this.modalService.openModal('create', CreateWalletComponent);
+    }
 
-  restore(): void {
-    this.modalService.openModal('restore', RestoreWalletComponent);
-  }
+    restore(): void {
+        this.modalService.openModal('restore', RestoreWalletComponent);
+    }
 
-  ngOnInit() {
-
-    this._AppService.configDataChangeObs$
-      .subscribe(data => {
-        if (data) {
-          this.configData = data;
-        }
-      });
-  }
+    ngOnInit() {    
+        this._AppService.configDataChangeObs$
+          .subscribe(data => {
+              if (data) {
+                  this.configData = data;
+              }
+          });
+    }
 
 
 }
