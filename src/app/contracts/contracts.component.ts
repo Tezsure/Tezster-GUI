eb49900109ea28024df068ba46dad5d955ca57e4
rmanjhi <rmanjhi.cse13@nituk.ac.in>
2019-09-30 01:41:52 +0530

    update CSS for UI

--- src/app/contracts/contracts.component.ts
+++ src/app/contracts/contracts.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {AppService} from '../app.service';
 import { timeout } from 'q';
-declare var eztz: any;
 
+declare var eztz: any;
 declare var conseiljs: any;
+
 @Component({
 	selector: 'app-contracts',
 	templateUrl: `./contracts.component.html`,
@@ -18,7 +19,7 @@ export class ContractsComponent implements OnInit {
 	result: any;
 	contractText: any;
 	tezosProvider: any;
-	StoreType:any;
+	StoreType: any;
 	public config;
 	keys: {
 		"sk": any;
@@ -40,7 +41,7 @@ export class ContractsComponent implements OnInit {
 	ngOnInit() {
 		this._AppService.configDataChangeObs$.subscribe(data => {
 			if (data) {
-				this.configData = data;				
+				this.configData = data;
 			}
 		});
 	}
@@ -63,43 +64,40 @@ export class ContractsComponent implements OnInit {
 		if (typeof this.accountpkh !== 'undefined' && typeof this.contract !== 'undefined' && typeof this.initValue !== 'undefined') {
 			this.config = JSON.parse(localStorage.getItem('configData'));
 			this.tezosProvider = this.config['provider'];
-			let accounts=this.findKeyObj(this.config['identities'],this.accountpkh);
-			if(!accounts){
+			let accounts = this.findKeyObj(this.config['identities'], this.accountpkh);
+			if (!accounts) {
 				alert(`Couldn't find keys for given account.
 				Please make sure the account exists and added to tezster. Run 'tezster list-accounts to get all accounts`);
-				
-			}else{
-				let cont=this.findKeyObj(this.config['contracts'],this.accountpkh);
-			if(cont){
-				alert(`This contract label is already in use. Please use a different one.`);
-			}
-			else{				
-			this.keys = {
-				"sk": accounts.sk,
-				"pk": accounts.pk,
-				"pkh": accounts.pkh,
-				"label": accounts.label
-			};
-			this.keysStore = {
-				publicKey: this.keys.pk,
-				privateKey: this.keys.sk,
-				publicKeyHash: this.keys.pkh,
-				seed: '',
-				storeType: conseiljs.StoreType.Fundraiser
-			};
-			
-			this.result = this._AppService.deployContract(this.accountpkh, this.contractText,
-				 this.contract, this.initValue, this.tezosProvider, this.keysStore);
-			setTimeout(() => {
-				// this.contract = '';
-				// this.initValue = '';																		
-				console.log(this.result);
-				alert(`If you're running a local node, Please run "tezster bake-for <account-name> to bake this operation
+
+			} else {
+				let cont = this.findKeyObj(this.config['contracts'], this.accountpkh);
+				if (cont) {
+					alert(`This contract label is already in use. Please use a different one.`);
+				} else {
+					this.keys = {
+						"sk": accounts.sk,
+						"pk": accounts.pk,
+						"pkh": accounts.pkh,
+						"label": accounts.label
+					};
+					this.keysStore = {
+						publicKey: this.keys.pk,
+						privateKey: this.keys.sk,
+						publicKeyHash: this.keys.pkh,
+						seed: '',
+						storeType: conseiljs.StoreType.Fundraiser
+					};
+
+					this.result = this._AppService.deployContract(this.accountpkh, this.contractText,
+						this.contract, this.initValue, this.tezosProvider, this.keysStore);
+					setTimeout(() => {																							
+						//console.log(this.result);
+						alert(`If you're running a local node, Please run "tezster bake-for <account-name> to bake this operation
 				\n If you're using alphanet node, use https://alphanet.tzscan.io to check contract/transactions`);
-				
-			},3000);
-		}
-		}			
+
+					}, 2000);
+				}
+			}
 		} else {
 			alert('Please select an account/Contract/Inital Contract Value !!');
 		}
@@ -108,66 +106,61 @@ export class ContractsComponent implements OnInit {
 		if (typeof this.accountpkh !== 'undefined' && typeof this.contract !== 'undefined' && typeof this.initValue !== 'undefined') {
 			this.config = JSON.parse(localStorage.getItem('configData'));
 			this.tezosProvider = this.config['provider'];
-			let accounts=this.findKeyObj(this.config['identities'],this.accountpkh);
-			if(!accounts){
+			let accounts = this.findKeyObj(this.config['identities'], this.accountpkh);
+			if (!accounts) {
 				alert(`Couldn't find keys for given account.
 				Please make sure the account exists and added to tezster. Run 'tezster list-accounts to get all accounts`);
-				
-			}else{			
-			this.keys = {
-				"sk": accounts.sk,
-				"pk": accounts.pk,
-				"pkh": accounts.pkh,
-				"label": accounts.label
-			};
-			this.keysStore = {
-				publicKey: this.keys.pk,
-				privateKey: this.keys.sk,
-				publicKeyHash: this.keys.pkh,
-				seed: '',
-				storeType: conseiljs.StoreType.Fundraiser
-			};
-			let cont=this.findKeyObj(this.config['contracts'],this.accountpkh);
-			if(!cont){
-				alert(`couldn't find the contract, please make sure contract label or address is correct!`);
+
+			} else {
+				this.keys = {
+					"sk": accounts.sk,
+					"pk": accounts.pk,
+					"pkh": accounts.pkh,
+					"label": accounts.label
+				};
+				this.keysStore = {
+					publicKey: this.keys.pk,
+					privateKey: this.keys.sk,
+					publicKeyHash: this.keys.pkh,
+					seed: '',
+					storeType: conseiljs.StoreType.Fundraiser
+				};
+				let cont = this.findKeyObj(this.config['contracts'], this.contract);
+				if (!cont) {
+					alert(`couldn't find the contract, please make sure contract label or address is correct!`);
+				} else {
+					let contractAdd = cont.pkh;
+					this.result = this._AppService.callContract(contractAdd, this.contract, this.initValue, this.tezosProvider, this.keysStore);
+					setTimeout(() => {
+						//console.log(this.result);
+					}, 2000);
+				}
 			}
-			else{
-			let contractAdd=cont.pkh;
-			this.result = this._AppService.callContract(contractAdd, this.contract, this.initValue, this.tezosProvider, this.keysStore);
-			setTimeout(() => {	
-				console.log(this.result);				
-			},3000);
-		}
-		}
 		} else {
 			alert('Please select an account/Contract/Inital Contract Value !!');
 		}
 	}
 	async getStorage() {
 		this.config = JSON.parse(localStorage.getItem('configData'));
-		if (typeof this.accountpkh != 'undefined' && typeof this.contract != 'undefined') {						
-			let cont=this.findKeyObj(this.config['contracts'],this.accountpkh);			
-			if(!cont){
+		if (typeof this.accountpkh != 'undefined' && typeof this.contract != 'undefined') {
+			let cont = this.findKeyObj(this.config['contracts'], this.contract);
+			if (!cont) {
 				alert(`couldn't find the contract, please make sure contract label or address is correct!`);
+			} else {
+				let contractAddress = cont.pkh;
+				this.result = this._AppService.getStorage(contractAddress);
+				setTimeout(() => {
+					//console.log(this.result);
+				}, 2000);
 			}
-			else{
-			let contractAddress=cont.pkh;
-			try {
-				let storage = await eztz.contract.storage(contractAddress);
-				alert (JSON.stringify(storage));
-			  }
-			  catch(error) {
-				alert(error);
-			  }							
-			}			
 		} else {
 			alert('Please select an account/Contract/Inital Contract Value !!');
 		}
 	}
 
-	findKeyObj(list, t){
-		for (var i = 0; i < list.length; i++){
-		  if (list[i].pkh == t || list[i].label == t) return list[i];
+	findKeyObj(list, t) {
+		for (var i = 0; i < list.length; i++) {
+			if (list[i].pkh == t || list[i].label == t) return list[i];
 		}
 		return false;
 	}
--- src/app/restore-wallet/restore-wallet.component.ts
+++ src/app/restore-wallet/restore-wallet.component.ts
@@ -3,6 +3,7 @@ import {AppService} from '../app.service';
 import { BsModalRef, BsModalService} from 'ngx-bootstrap/modal';
 import { ModalService } from '../modal.service';
 import { Router } from '@angular/router';
+
 declare var eztz: any;
 
 @Component({
@@ -11,70 +12,83 @@ declare var eztz: any;
     styleUrls: ['./restore-wallet.component.css']
 })
 export class RestoreWalletComponent implements OnInit {
-    public configData=[];
+    public configData = [];
     public data: string;
     public mnenomics: any;
     public password: any;
-    public email: any;  
-    public cred:string;
-    public keys: { pkh: string; sk: any; pk: any; };
+    public email: any;
+    public cred: string;
+    public keys: {
+        pkh: string;sk: any;pk: any;
+    };
     public account: string;
     public secret: any;
-    credD: { "pkh": any; "sk": any; "pk": any; label: any };
-    public obj;	
+    credD: {
+        "pkh": any;
+        "sk": any;
+        "pk": any;label: any
+    };
+    public obj;
     public label;
-    constructor(public bsModalRef: BsModalRef, private _AppService:AppService, private modalService: ModalService,private router: Router ) { }
-    
-  
-  onNoClick(): void {     
-      this.modalService.closeModal('restore');
-  }
+    constructor(public bsModalRef: BsModalRef, private _AppService: AppService, private modalService: ModalService, private router: Router) {}
 
-  onClick(): void {
-      this.cred=this.email+this.password;  
-      this.keys=eztz.crypto.generateKeys(this.mnenomics, this.cred); 
-      this.account=this.keys.pkh;            
-      this._AppService.addAccount(
-          {
-              "label": this.label,
-              "pkh": this.account,
-              "identity": this.label
-          }
-      );     
-      this.credD={"pkh":this.keys.pkh,"sk":this.keys.sk,"pk":this.keys.pk ,"label":this.label};		
-      this.obj=JSON.parse(this._AppService.getLocalConfigData());
-      this.obj.identities.push(this.credD);
-      this._AppService.setLocalConfigData(this.obj);
-      this.modalService.closeModal('restore');
-      alert("An account "+this.label+ " has been restored with Identity - " + this.account);
-      this.router.navigate(['/accounts']);   
-  }
-  activateAlphanet(): void{	 
-      this.cred=this.email+this.password;  
-      this.keys=eztz.crypto.generateKeys(this.mnenomics, this.cred); 
-      this.account=this.keys.pkh;	          
-      this._AppService.addAccount(
-            {
-              "label": this.label,
-              "pkh": this.account,
-              "identity": this.label
-            }
-      );           
-      this.credD={"pkh":this.keys.pkh,"sk":this.keys.sk,"pk":this.keys.pk ,"label":this.label};		
-      this.obj=JSON.parse(this._AppService.getLocalConfigData());
-      this.obj.identities.push(this.credD);
-      this._AppService.setLocalConfigData(this.obj);
-      this.modalService.closeModal('restore'); 
-      this.router.navigate(['/accounts']);   
-  }
-  ngOnInit() { 
-    
-      this._AppService.configDataChangeObs$
-      .subscribe(data => {       
-          if (data) {
-              this.configData = data;  
-          }
-      });
-  }
+
+    onNoClick(): void {
+        this.modalService.closeModal('restore');
+    }
+
+    onClick(): void {
+        this.cred = this.email + this.password;
+        this.keys = eztz.crypto.generateKeys(this.mnenomics, this.cred);
+        this.account = this.keys.pkh;
+        this._AppService.addAccount({
+            "label": this.label,
+            "pkh": this.account,
+            "identity": this.label
+        });
+        this.credD = {
+            "pkh": this.keys.pkh,
+            "sk": this.keys.sk,
+            "pk": this.keys.pk,
+            "label": this.label
+        };
+        this.obj = JSON.parse(this._AppService.getLocalConfigData());
+        this.obj.identities.push(this.credD);
+        this._AppService.setLocalConfigData(this.obj);
+        this.modalService.closeModal('restore');
+        alert("An account " + this.label + " has been restored with Identity - " + this.account);
+        this.router.navigate(['/accounts']);
+    }
+    activateAlphanet(): void {
+        this.cred = this.email + this.password;
+        this.keys = eztz.crypto.generateKeys(this.mnenomics, this.cred);
+        this.account = this.keys.pkh;
+        this._AppService.addAccount({
+            "label": this.label,
+            "pkh": this.account,
+            "identity": this.label
+        });
+        this.credD = {
+            "pkh": this.keys.pkh,
+            "sk": this.keys.sk,
+            "pk": this.keys.pk,
+            "label": this.label
+        };
+        this.obj = JSON.parse(this._AppService.getLocalConfigData());
+        this.obj.identities.push(this.credD);
+        this._AppService.setLocalConfigData(this.obj);
+        alert(`successfully restored alphanet faucet account: ${this.label}-${this.account}`);
+        this.modalService.closeModal('restore');
+        this.router.navigate(['/accounts']);
+    }
+    ngOnInit() {
+
+        this._AppService.configDataChangeObs$
+            .subscribe(data => {
+                if (data) {
+                    this.configData = data;
+                }
+            });
+    }
 
 }
