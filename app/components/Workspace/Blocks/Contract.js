0629c97f3b7b62f703d6e204dfda35d0323c9d17
aditya-tezsure <aditya@tezsure.com>
2020-07-13 20:40:48 +0530

    adding support for contract explorer api

--- app/components/Workspace/Blocks/Contract.js
+++ app/components/Workspace/Blocks/Contract.js
@@ -15,10 +15,21 @@ class Contracts extends Component {
   }
 
   render() {
-    const ContractsData = this.props.hasOwnProperty('blockSearch')
-      ? this.props.blockSearch
-      : [];
-    const contracts = ContractsData.map((elem, index) => {
+    const { blockSearch } = this.props;
+    const ContractsData = () => {
+      switch (true) {
+        case blockSearch[0] === undefined &&
+          Object.keys(blockSearch).length > 0:
+          return [blockSearch];
+        case blockSearch[0] === undefined:
+          return [];
+        case blockSearch.length > 0:
+          return blockSearch;
+        default:
+          return [];
+      }
+    };
+    const contracts = ContractsData().map((elem, index) => {
       return (
         <div key={elem.address + index} className="block-cards">
           <div className="block-contents">
@@ -26,13 +37,15 @@ class Contracts extends Component {
               <b>Address:</b> {`${elem.address}`}
             </div>
             <div className="gas-limit details-container">
-              <b>Delegate:</b> {`${elem.delegate}`}
+              <b>Delegate:</b> {`${elem.delegate}` || 'N/A'}
             </div>
             <div className="time details-container">
               <b>Gas-used:</b> {`${elem.gas_used}`}
+              &nbsp;<span className="tezos-icon">ꜩ</span>
             </div>
             <div className="hash details-container">
               <b>Gas-price:</b> {`${elem.gas_price}`}
+              &nbsp;<span className="tezos-icon">ꜩ</span>
             </div>
             <div className="type details-container">
               <b>Fee:</b> {`${elem.fee || 'N/A'}`}
